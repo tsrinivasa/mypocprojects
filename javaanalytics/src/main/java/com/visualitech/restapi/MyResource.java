cb88a66aa08651ddb4d15cac50ebee5b6f0469dd
tsrinivasa <bijavaexpert@gmail.com>
2016-02-20 15:54:57 +0530

    Added infor of Fonts

--- javaanalytics/src/main/java/com/visualitech/restapi/MyResource.java
+++ javaanalytics/src/main/java/com/visualitech/restapi/MyResource.java
@@ -5,6 +5,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+/**
+Designed by Srinivasa
+*/
+
 /** Example resource class hosted at the URI path "/myresource"
  */
 @Path("/myresource")
